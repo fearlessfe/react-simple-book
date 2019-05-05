d7e45c08159260db0ed86f13f774a46decd5c103
fearlessfe <505380967@qq.com>
2019-05-05 21:50:42 +0800

    redux基本用法

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,70 +1,66 @@
-import React, { Component, Fragment } from 'react'
-import TodoItem from './TodoItem'
+import React, { Component } from 'react'
+import { Input, Button, List } from 'antd'
+import 'antd/dist/antd.css';
+import store from './store'
+import {
+  getInputChangeAction,
+  getAddItemAction,
+  getDeleteItemAction
+} from './store/actionCreator'
 
 class TodoList extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: '',
-      list: ['学习React', '学习Angular']
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.submit = this.submit.bind(this)
+    super(props)
+    this.state = store.getState()
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
     this.deleteItem = this.deleteItem.bind(this)
-    this.getTodoItem = this.getTodoItem.bind(this)
-  }
 
-  handleChange(event) {
-    const inputValue = event.target.value
-    this.setState(() => (
-      {
-        inputValue
-      }
-    ))
-  }
-
-  submit() {
-    this.setState((prevState) => 
-      ({
-        list: [...prevState.list, prevState.inputValue],
-        inputValue: ''
-      })
-    )
-  }
-
-  deleteItem(index) {
-    this.setState((prevState) => {
-      const list = [...prevState.list]
-      list.splice(index, 1)
-      return {
-        list
-      }
-    })
-  }
-
-  getTodoItem() {
-    const { list } = this.state
-    return list.map((item, index) => {
-      return <TodoItem key={index} item={item} index={index} deleteItem={this.deleteItem.bind(this)} />
-    })
+    store.subscribe(this.handleStoreChange)
   }
 
   render() {
-    const { inputValue } = this.state
+    const { inputValue, list } = this.state
     return (
-      <Fragment>
+      <div style={{marginTop: '10px', marginLeft: '10px'}}>
         <div>
-          <input onChange={this.handleChange} value={inputValue} placeholder="请输入" />
-          <button onClick={this.submit}>提交</button>
+          <Input 
+            value={inputValue} 
+            placeholder="Basic usage" 
+            style={{width: '300px', marginRight: '10px'}}
+            onChange={this.handleInputChange}
+          />
+          <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
         </div>
-        <ul>
-          {
-            this.getTodoItem()
-          }
-        </ul>
-      </Fragment>
+        <List
+          style={{width: '300px', marginTop: '10px'}}
+          bordered
+          dataSource={list}
+          renderItem={(item,index) => (<List.Item onClick={() => {this.deleteItem(index)}}>{item}</List.Item>)}
+        />
+      </div>
     )
   }
-}
 
+  handleInputChange(e) {
+    const action = getInputChangeAction(e.target.value)
+    store.dispatch(action)
+  }
+
+  handleBtnClick() {
+    const action = getAddItemAction()
+    store.dispatch(action)
+  }
+
+  deleteItem(index) {
+    const action = getDeleteItemAction(index)
+    store.dispatch(action)
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState())
+  }
+}
 export default TodoList;
